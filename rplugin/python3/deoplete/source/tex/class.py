b0651ecf357b8681efd1294f603dbfa341b6b2e2
E Kawashima <e-kwsm@users.noreply.github.com>
2018-04-27 20:34:42 +0900

    update rplugin/python3/deoplete/source/tex

--- rplugin/python3/deoplete/source/tex/class.py
+++ rplugin/python3/deoplete/source/tex/class.py
@@ -14,6 +14,7 @@ class Source(Base):
             "article",
             "beamer",
             "book",
+            "jlreq",
             "minimal",
             "report",
             "scrartcl",
