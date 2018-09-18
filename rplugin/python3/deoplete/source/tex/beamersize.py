a41b16a18e222a6ad03e2c76d03dd0d0ca530621
E Kawashima <e-kwsm@users.noreply.github.com>
2018-09-18 17:37:08 +0900

    implement rplugin/python3/deoplete/source/tex/beamersize.py

---
+++ rplugin/python3/deoplete/source/tex/beamersize.py
@@ -0,0 +1,28 @@
+import re
+from .base import Base
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = "beamer.size"
+        self.mark = "[beamer.size]"
+        self.filetypes = ["tex"]
+        self.input_pattern = r"\\setbeamersize\{[^\}]*$"
+        self._candidate = [{
+            "word": s + "=",
+            "abbr": s,
+        } for s in [
+            "description width of",
+            "description width",
+            "mini frame offset",
+            "mini frame size",
+            "sidebar width left",
+            "sidebar width right",
+            "text margin left",
+            "text margin right",
+        ]]
+
+    def gather_candidates(self, context):
+        if re.search(self.input_pattern, context["input"]):
+            return self._candidate
