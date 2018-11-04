5890eefca3a1ce81e417f5c9994d8c915fb018e5
E Kawashima <e-kwsm@users.noreply.github.com>
2018-11-04 11:14:28 +0900

    implement rplugin/python3/deoplete/source/tex/tikzkeyhandler.py

---
+++ rplugin/python3/deoplete/source/tex/tikzkeyhandler.py
@@ -0,0 +1,78 @@
+import re
+from .base import Base
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = "tikz key handlers"
+        self.mark = "[tikz]"
+        self.filetypes = ["tex"]
+        self.input_pattern = r"\w+/\.[a-z]*$"
+        self._candidates = []
+        for h in [
+            "activate family",
+            "add code=",
+            "add style=",
+            "add=",
+            "append code=",
+            "append style=",
+            "append=",
+            "belongs to family=",
+            "cd",
+            "code 2 args=",
+            "code args=",
+            "code n args=",
+            "code=",
+            "deactivate family",
+            "default=",
+            "ecode 2 args=",
+            "ecode args=",
+            "ecode n args=",
+            "ecode=",
+            "estore in=",
+            "estyle 2 args=",
+            "estyle args=",
+            "estyle=",
+            "expand once=",
+            "expand twice=",
+            "expanded=",
+            "forward to=",
+            "get=",
+            "initial=",
+            "install key filter handler=",
+            "install key filter=",
+            "is choice",
+            "is family",
+            "is if=",
+            "lastretry=",
+            "link=",
+            "list=",
+            "pic=",
+            "prefix code=",
+            "prefix style=",
+            "prefix=",
+            "retry=",
+            "search also=",
+            "show code",
+            "show value",
+            "store in=",
+            "style 2 args=",
+            "style args=",
+            "style n args=",
+            "style sheet=",
+            "style=",
+            "tip=",
+            "try=",
+            "value forbidden",
+            "value required",
+        ]:
+            self._candidates += [{
+                "word": h,
+                "kind": "key handler",
+                "abbr": "." + h,
+            }]
+
+    def gather_candidates(self, context):
+        if re.search(self.input_pattern, context["input"]):
+            return self._candidates
