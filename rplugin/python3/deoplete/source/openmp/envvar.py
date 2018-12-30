eeb2478376dffe3f737f19a3867a9002274b8ef1
E Kawashima <e-kwsm@users.noreply.github.com>
2018-12-30 16:19:46 +0900

    update rplugin/python3/deoplete/source/{openmp/envvar.py,pbs.py,slurm.py}

--- rplugin/python3/deoplete/source/openmp/envvar.py
+++ rplugin/python3/deoplete/source/openmp/envvar.py
@@ -1,3 +1,4 @@
+import re
 from .base import Base
 
 
@@ -7,22 +8,23 @@ class Source(Base):
         self.name = "OpenMP environment variables"
         self.mark = "[OpenMP]"
         self.filetypes = ["sh"]
-        self.input_pattern = r"(?:\bexport\s+|\$\{?)?[A-Z_]*$"
+        self.input_pattern = r"(?:\bexport\s+|\$\{?)?OMP[A-Z_]*$"
 
     def gather_candidates(self, context):
-        return [
-            "OMP_CANCELLATION",
-            "OMP_DEFAULT_DEVICE",
-            "OMP_DISPLAY_ENV",
-            "OMP_DYNAMIC",
-            "OMP_MAX_ACTIVE_LEVELS",
-            "OMP_MAX_TASK_PRIORITY",
-            "OMP_NESTED",
-            "OMP_NUM_THREADS",
-            "OMP_PLACES",
-            "OMP_PROC_BIND",
-            "OMP_SCHEDULE",
-            "OMP_STACKSIZE",
-            "OMP_THREAD_LIMIT",
-            "OMP_WAIT_POLICY",
-        ]
+        if re.search(self.input_pattern, context["input"]):
+            return [
+                "OMP_CANCELLATION",
+                "OMP_DEFAULT_DEVICE",
+                "OMP_DISPLAY_ENV",
+                "OMP_DYNAMIC",
+                "OMP_MAX_ACTIVE_LEVELS",
+                "OMP_MAX_TASK_PRIORITY",
+                "OMP_NESTED",
+                "OMP_NUM_THREADS",
+                "OMP_PLACES",
+                "OMP_PROC_BIND",
+                "OMP_SCHEDULE",
+                "OMP_STACKSIZE",
+                "OMP_THREAD_LIMIT",
+                "OMP_WAIT_POLICY",
+            ]
