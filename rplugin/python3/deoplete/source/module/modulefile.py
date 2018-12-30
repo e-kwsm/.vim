5de5bf4482092f6409768438f70189eb5a07f5f7
E Kawashima <e-kwsm@users.noreply.github.com>
2018-12-30 15:14:57 +0900

    implement rplugin/python3/deoplete/source/module/

---
+++ rplugin/python3/deoplete/source/module/modulefile.py
@@ -0,0 +1,53 @@
+import re
+import subprocess
+from pathlib import Path
+from .base import Base
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = "modulefile"
+        self.mark = "[modulefile]"
+        self.filetypes = ["module"]
+        self.input_pattern = r"^\s*module\s+(?:" + "|".join([
+            "add",
+            "conflict",
+            "display",
+            "help",
+            "initadd",
+            "initprepend",
+            "initrm",
+            "initswitch",
+            "is-loaded",
+            "load",
+            "prereq",
+            "rm",
+            "show",
+            "swap",
+            "switch",
+            "unload",
+        ]) + r")\s+"
+
+        p = subprocess.run("modulecmd python avail -t".split(),
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.PIPE, universal_newlines=True)
+        self._candidates = []
+        d = None
+        for m in p.stderr.splitlines():
+            if m.endswith(":"):
+                d = m[:-1]
+                continue
+            for p in list(Path(m).parents)[-2::-1]:
+                self._candidates += [{
+                    "word": str(p),
+                    "kind": d or "",
+                }]
+            self._candidates += [{
+                "word": m,
+                "kind": d or "",
+            }]
+
+    def gather_candidates(self, context):
+        if re.search(self.input_pattern, context["input"]):
+            return self._candidates
