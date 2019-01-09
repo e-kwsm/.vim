ad2736b527aeef4bcdd0354cbb6eb8fd5eefbdc9
E Kawashima <e-kwsm@users.noreply.github.com>
2019-01-09 21:14:18 +0900

    implement rplugin/python3/deoplete/source/tex/minted.py

---
+++ rplugin/python3/deoplete/source/tex/minted.py
@@ -0,0 +1,27 @@
+import re
+from pygments.lexers import get_all_lexers
+from .base import Base
+
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = "pygments.lexer"
+        self.mark = "[pygments.lexer]"
+        self.filetypes = ["tex"]
+        self.input_pattern = r"\\(?:" + "|".join([
+            r"begin{minted}",
+            r"mint",
+            r"mintinline",
+            r"inputminted(?:\[.*?\])?",
+        ]) + r"){\S*$"
+        self.rank = 800
+
+        self._candidates = []
+        for lexer in get_all_lexers():
+            self._candidates += list(lexer[1])
+        self._candidates.sort()
+
+    def gather_candidates(self, context):
+        if re.search(self.input_pattern, context["input"]):
+            return self._candidates
