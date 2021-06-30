2cc0328fafc2ecc72086f3255c98b4586d08aef0
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-06-30 12:13:29 +0900

    spell/update.py

--- spell/update.py
+++ spell/update.py
@@ -6,11 +6,11 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 
-def update(vim: str = "nvim", check: bool = False):
+def update(vim: str = "nvim", check: bool = False, casesensitive: bool = True):
     add = "en.utf-8.add"
     ref = NamedTemporaryFile(delete=False)
 
-    cmd = f"{vim} -u NONE".split()
+    cmd = [vim, "-u", "NONE"]
     if Path(vim).name == "nvim":
         cmd.append("--headless")
     cmd.append("-c{}".format("|".join([
@@ -19,9 +19,9 @@ def update(vim: str = "nvim", check: bool = False):
         "sort u",
         "up",
         "mkspell! %",
-        "%bd",
+        "%bw",
         "spelldump",
-        "0;2/^#/d",
+        "keepp 0;2/^#/d",
         f"w! {ref.name}",
         "qa",
     ])))
@@ -31,25 +31,31 @@ def update(vim: str = "nvim", check: bool = False):
         Path(ref.name).unlink()
         return
 
-    newwords = []
+    pat = re.compile("/[^/]+$")
+
+    newwords = set()
     with open(add) as f:
         for w in f:
-            newwords.append(re.sub(r"(?:/.*)?\n$", "", w))
-    newwords = "|".join(newwords)
+            if not w.startswith("#") and not w.startswith("/"):
+                w = pat.sub("", w.strip())
+                newwords.add(w if casesensitive else w.lower())
+
     with open(ref.name) as f:
-        for word in f:
-            word = word.replace(".", r"\.")
-            m = re.search(fr"^(?:{newwords})\b", word, flags=re.IGNORECASE)
-            if m:
-                print(word.replace("\n", ""), m, sep="\t")
+        for w in f:
+            w = pat.sub("", w.strip())
+            if not casesensitive:
+                w = w.lower()
+            if w in newwords:
+                print(w)
+
     Path(ref.name).unlink()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("vim", nargs="?", default="nvim", help="vim/nvim")
-    parser.add_argument("--check",
-                        "-c",
+    parser.add_argument("-c",
+                        "--check",
                         action="store_true",
                         help="check duplicates")
     args = parser.parse_args()
