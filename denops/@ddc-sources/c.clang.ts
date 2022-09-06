cc5074b281e981b05a77582c36ff85e3dc135409
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-09-07 03:22:38 +0900

    denops/@ddc-sources/c.clang.ts

---
+++ denops/@ddc-sources/c.clang.ts
@@ -0,0 +1,32 @@
+import {
+  BaseSource,
+  DdcGatherItems,
+} from "https://deno.land/x/ddc_vim@v6.0.0/types.ts#^";
+import { GatherArguments } from "https://deno.land/x/ddc_vim@v6.0.0/base/source.ts#^";
+
+type Params = Record<string, never>;
+
+export class Source extends BaseSource<Params> {
+  async gather(
+    args: GatherArguments<Params>,
+  ): Promise<DdcGatherItems> {
+    if (!args.context.input.match(/^\s*#\s*pragma\s+clang\s+diagnostic\s+/)) {
+      return [];
+    }
+
+    return await Promise.all([
+      "error",
+      "fatal",
+      "ignored",
+      "pop",
+      "push",
+      "warning",
+    ].map(
+      (word) => Promise.resolve({ menu: "diagnostic", word: word, abbr: word }),
+    ));
+  }
+
+  params(): Params {
+    return {};
+  }
+}
