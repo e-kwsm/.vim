c879f744da7ec222aa429866676abea2ada9f5e6
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-11-17 15:18:10 +0900

    denops/@ddc-sources/

--- denops/@ddc-sources/rst.directive.ts
+++ denops/@ddc-sources/rst.directive.ts
@@ -2,8 +2,8 @@ import {
   BaseSource,
   DdcGatherItems,
   Item,
-} from "https://deno.land/x/ddc_vim@v4.0.5/types.ts#^";
-import { GatherArguments } from "https://deno.land/x/ddc_vim@v4.0.5/base/source.ts#^";
+} from "https://deno.land/x/ddc_vim@v4.1.0/types.ts#^";
+import { GatherArguments } from "https://deno.land/x/ddc_vim@v4.1.0/base/source.ts#^";
 
 type Params = Record<string, never>;
 
