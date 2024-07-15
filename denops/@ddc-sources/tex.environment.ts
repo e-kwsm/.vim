d4e97e1927d773b8db27846a340b8e3efb7ede95
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-16 06:35:11 +0900

    denops/@ddc-sources/

--- denops/@ddc-sources/tex.environment.ts
+++ denops/@ddc-sources/tex.environment.ts
@@ -3,8 +3,8 @@ import {
   BaseSource,
   DdcGatherItems,
   Item,
-} from "https://deno.land/x/ddc_vim@v4.3.1/types.ts#^";
-import { GatherArguments } from "https://deno.land/x/ddc_vim@v4.3.1/base/source.ts#^";
+} from "https://deno.land/x/ddc_vim@v5.0.1/types.ts#^";
+import { GatherArguments } from "https://deno.land/x/ddc_vim@v5.0.1/base/source.ts#^";
 
 async function _generate(pkg: string, envs: string[]): Promise<Item[]> {
   return await Promise.all(envs.map(
