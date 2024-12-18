fed7071ba73044305a2a88a0a554142327bff0e4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-18 19:20:46 +0900

    denops/@ddc-sources/svg.font.ts

--- denops/@ddc-sources/svg.font.ts
+++ denops/@ddc-sources/svg.font.ts
@@ -1,38 +1,38 @@
+import { Item } from "jsr:@shougo/ddc-vim@^9.1.0/types";
 import {
   BaseSource,
-  DdcGatherItems,
-} from "https://deno.land/x/ddc_vim@v6.0.0/types.ts#^";
-import {
   GatherArguments,
   OnInitArguments,
-} from "https://deno.land/x/ddc_vim@v6.0.0/base/source.ts#^";
+} from "jsr:@shougo/ddc-vim@^9.1.0/source";
 
 type Params = Record<string, never>;
 
 export class Source extends BaseSource<Params> {
   candidates: string[] = [];
 
-  async onInit(_args: OnInitArguments<Params>): Promise<void> {
+  override async onInit(_args: OnInitArguments<Params>): Promise<void> {
     const command = new Deno.Command("fc-list", {
       args: ["--format", "%{family[0]}\n"],
       env: { "LANG": "C" },
       stdin: "null",
       stdout: "piped",
     });
-    const { _code, stdout, _stderr } = await command.output();
-    this.candidates = new TextDecoder().decode(stdout).split(/\n/);
+    const { stdout } = await command.output();
+    this.candidates = new TextDecoder().decode(stdout)
+      .split(/\n/);
   }
 
-  async gather(args: GatherArguments<Params>): Promise<DdcGatherItems> {
+  override async gather(args: GatherArguments<Params>): Promise<Item[]> {
     if (!args.context.input.match(/\bfont-\w+\b.*/)) {
       return [];
     }
-    return await Promise.all(this.candidates.map(
-      (word) => Promise.resolve({ menu: "font", word: word }),
-    ));
+    const items = await Promise.all(
+      this.candidates.map((word) => ({ menu: "font", word: word })),
+    );
+    return items;
   }
 
-  params(): Params {
+  override params(): Params {
     return {};
   }
 }
