d0cd53fe0bd0843e6d7bc7931c974347b290b00c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-20 16:53:31 +0900

    init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,12 @@ local vimrc = vim.env.HOME .. "/.vim/vimrc"
 if vim.uv.fs_stat(vimrc) then
   vim.cmd.source(vimrc)
 end
+
+vim.o.inccommand = "split"
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  command = "startinsert",
+})
+
+vim.api.nvim_create_user_command("Exe", "up | te chmod u+x % && %:p <args>", { nargs = "*" })
