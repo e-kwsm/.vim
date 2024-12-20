99fce2f2ac06350cbe0536eafcb2bbf97a697899
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-20 16:53:31 +0900

    init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ local vimrc = vim.env.HOME .. "/.vim/vimrc"
 if vim.uv.fs_stat(vimrc) then
   vim.cmd.source(vimrc)
 end
+
+vim.o.inccommand = "split"
+vim.o.spellfile = os.getenv("XDG_CONFIG_HOME") .. "/nvim/spell/en.utf-8.add"
