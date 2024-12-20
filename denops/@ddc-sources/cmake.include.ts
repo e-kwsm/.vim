db6b369659fb5a575ff37cdc2bda415e46d76891
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-20 10:04:58 +0900

    denops/@ddc-sources/

--- denops/@ddc-sources/cmake.include.ts
+++ denops/@ddc-sources/cmake.include.ts
@@ -1,11 +1,9 @@
+import { Item } from "jsr:@shougo/ddc-vim@^9.1.0/types";
 import {
   BaseSource,
-  DdcGatherItems,
-} from "https://deno.land/x/ddc_vim@v6.0.0/types.ts#^";
-import {
   GatherArguments,
   OnInitArguments,
-} from "https://deno.land/x/ddc_vim@v6.0.0/base/source.ts#^";
+} from "jsr:@shougo/ddc-vim@^9.1.0/source";
 
 type Params = Record<string, never>;
 
@@ -24,17 +22,17 @@ export class Source extends BaseSource<Params> {
       .filter((line) => !line.match("^Find"));
   }
 
-  async gather(args: GatherArguments<Params>): Promise<DdcGatherItems> {
+  override async gather(args: GatherArguments<Params>): Promise<Item[]> {
     if (!args.context.input.match(/\binclude\(\w*$/i)) {
       return [];
     }
-    const items = await Promise.all(this.candidates.map(
+    const items: Item[] = await Promise.all(this.candidates.map(
       (word) => Promise.resolve({ menu: "include", word: word }),
     ));
     return items;
   }
 
-  params(): Params {
+  override params(): Params {
     return {};
   }
 }
