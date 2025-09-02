c1de08948a63335dd4abd667144796144cb8c39e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-03 02:39:25 +0900

    lua/config/lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -106,8 +106,9 @@ require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./vsnip" } })
 
 -- neovim/nvim-lspconfig {{{1
 vim.lsp.enable("clangd")
+vim.lsp.enable("codebook")
 vim.lsp.enable("denols")
--- vim.lsp.enable("hls")
+vim.lsp.enable("hls")
 vim.lsp.enable("pylsp")
 
 -- Shougo/ddc.vim {{{1
