0e3d36dcca6a039b1da914ecdf90d23e86b89c8f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-03-02 12:54:12 +0900

    init.lua

--- init.lua
+++ init.lua
@@ -5,3 +5,7 @@ end
 
 vim.o.inccommand = "split"
 vim.o.spellfile = os.getenv("XDG_CONFIG_HOME") .. "/nvim/spell/en.utf-8.add"
+if vim.o.diff then
+  vim.o.autoread = false
+  vim.opt.diffopt:remove("linematch:40")
+end
